791b081a46b20f1e05d2cd040a8d87fd69009d9b
akm <akm2000@gmail.com>
2021-02-02 20:09:59 +0900

    フローデザイナーのURLの検証を追加

--- playwright-handson/login.spec.js
+++ playwright-handson/login.spec.js
@@ -21,6 +21,7 @@ it('can login', async () => {
   const loginURL = process.env["LOGIN_URL"]
   const loginEmail = process.env["LOGIN_EMAIL"]
   const loginPassword = process.env['LOGIN_PASSWORD']
+  const expectedURL = process.env["EXPECTED_URL"]
 
   const browser = await chromium.launch({ headless: false, slowMo: 50 });
   const page = await browser.newPage();
@@ -31,6 +32,15 @@ it('can login', async () => {
   await page.click("#login-button");
 
   expect(await page.isVisible('.header__Items >> text="ダッシュボード"')).toBeTruthy()
+
+  await page.click('.header__Items > :nth-child(1) > .material-icons')
+  await page.click('#global-menu-bigdata > .menu2__item > .menu2__item--text > .main')
+
+  expect(await page.isVisible('.header__Items >> text="フローデザイナー"')).toBeTruthy()
+
+  expect(await page.isVisible('.boardList2__name > a.linkTo')).toBeTruthy()
+  const href = await page.$eval(".boardList2__name > a.linkTo", el => el.href)
+  expect(href).toEqual(expectedURL)
 
   await page.waitForTimeout(10 * 1000);
 
