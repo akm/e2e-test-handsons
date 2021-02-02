2419286dec775e0409b3c0a94d10106ccb32180b
akm <akm2000@gmail.com>
2021-02-02 19:10:17 +0900

    jestのテストとして実装

--- playwright-handson/login.spec.js
+++ playwright-handson/login.spec.js
@@ -1,7 +1,23 @@
 const { chromium } = require('playwright');
+const expect = require('expect');
 
+let browser;
+let page;
 
-(async () => {
+beforeAll(async () => {
+  browser = await chromium.launch({ headless: false, slowMo: 50 });
+});
+afterAll(async () => {
+  await browser.close();
+});
+beforeEach(async () => {
+  page = await browser.newPage();
+});
+afterEach(async () => {
+  await page.close();
+});
+
+it('can login', async () => {
   const loginURL = process.env["LOGIN_URL"]
   const loginEmail = process.env["LOGIN_EMAIL"]
   const loginPassword = process.env['LOGIN_PASSWORD']
@@ -14,7 +30,9 @@ const { chromium } = require('playwright');
   await page.fill("#input-password", loginPassword);
   await page.click("#login-button");
 
-  await page.waitForTimeout(30 * 1000);
+  expect(await page.isVisible('.header__Items >> text="ダッシュボード"')).toBeTruthy()
+
+  await page.waitForTimeout(10 * 1000);
 
   await browser.close();
-})();
+});
