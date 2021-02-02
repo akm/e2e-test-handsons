a33a3ec7ae48bbc40eb76e88b31933ddfc4607f2
akm <akm2000@gmail.com>
2021-02-02 17:22:00 +0900

    フローデザイナーの画面を開いてリンク先の検証を追加

--- cypress-handon/cypress/integration/login.js
+++ cypress-handon/cypress/integration/login.js
@@ -3,6 +3,7 @@
 const loginURL = Cypress.env("LOGIN_URL")
 const loginEmail = Cypress.env("LOGIN_EMAIL")
 const loginPassword = Cypress.env("LOGIN_PASSWORD")
+const expectedURL = Cypress.env("EXPECTED_URL")
 
 context("Login test", () => {
   it("login", () => {
@@ -15,5 +16,18 @@ context("Login test", () => {
       .get("#login-button")
       .click();
     cy.get(".header__Items").contains("ダッシュボード").should('be.visible')
+
+    cy.get('.header__Items > :nth-child(1) > .material-icons').click()
+    cy.get('#global-menu-bigdata > .menu2__item > .menu2__item--text > .main').click()
+
+    cy.get(".header__Items").contains("フローデザイナー").should('be.visible')
+
+    // cy.get(".boardList2__name > a.linkTo").its("href").should("equal", expectedURL)
+    cy.get(".boardList2__name > a.linkTo").then(($elements) => {
+      expect($elements).to.have.length(1)
+      expect($elements[0].href).to.equal(expectedURL)
+    })
+
+    // cy.get(".boardList2__name > a.linkTo").click()
   });
 });
