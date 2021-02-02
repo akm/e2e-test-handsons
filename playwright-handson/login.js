ab950207b08c065b1b77a608dfaba40d2c348845
akm <akm2000@gmail.com>
2021-02-02 18:57:40 +0900

    ログインする操作を実装

--- playwright-handson/login.js
+++ playwright-handson/login.js
@@ -1,9 +1,20 @@
-const { firefox } = require('playwright');
+const { chromium } = require('playwright');
+
 
 (async () => {
-  const browser = await firefox.launch({ headless: false, slowMo: 50 });
+  const loginURL = process.env["LOGIN_URL"]
+  const loginEmail = process.env["LOGIN_EMAIL"]
+  const loginPassword = process.env['LOGIN_PASSWORD']
+
+  const browser = await chromium.launch({ headless: false, slowMo: 50 });
   const page = await browser.newPage();
-  await page.goto('http://whatsmyuseragent.org/');
-  await page.screenshot({ path: `example.png` });
+  await page.goto(loginURL);
+
+  await page.fill("#input-email", loginEmail);
+  await page.fill("#input-password", loginPassword);
+  await page.click("#login-button");
+
+  await page.waitForTimeout(30 * 1000);
+
   await browser.close();
 })();
